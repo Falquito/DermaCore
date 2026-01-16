f3397850d5bae3401b6c2a07f6a10dd334d3b338
Cali <agustincalisaya01@gmail.com>
2026-01-16 20:46:04 -0300

    Agregue nroAfiliado en PacienteXObra

--- prisma/migrations/20260116234405_init/migration.sql
+++ prisma/migrations/20260116234405_init/migration.sql
@@ -38,10 +38,59 @@ CREATE TABLE "Consultas" (
 -- CreateTable
 CREATE TABLE "PacienteXObra" (
     "idPacientexObra" SERIAL NOT NULL,
+    "nroAfiliado" TEXT,
     "idPaciente" INTEGER NOT NULL,
     "idObraSocial" INTEGER NOT NULL,
 
     CONSTRAINT "PacienteXObra_pkey" PRIMARY KEY ("idPacientexObra")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sessionToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
 );
 
 -- CreateIndex
@@ -62,6 +111,27 @@ CREATE INDEX "PacienteXObra_idObraSocial_idx" ON "PacienteXObra"("idObraSocial")
 -- CreateIndex
 CREATE UNIQUE INDEX "PacienteXObra_idPaciente_idObraSocial_key" ON "PacienteXObra"("idPaciente", "idObraSocial");
 
+-- CreateIndex
+CREATE INDEX "Account_userId_idx" ON "Account"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE INDEX "Session_userId_idx" ON "Session"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
+
 -- AddForeignKey
 ALTER TABLE "Consultas" ADD CONSTRAINT "Consultas_idPaciente_fkey" FOREIGN KEY ("idPaciente") REFERENCES "Paciente"("idPaciente") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -73,3 +143,9 @@ ALTER TABLE "PacienteXObra" ADD CONSTRAINT "PacienteXObra_idPaciente_fkey" FOREI
 
 -- AddForeignKey
 ALTER TABLE "PacienteXObra" ADD CONSTRAINT "PacienteXObra_idObraSocial_fkey" FOREIGN KEY ("idObraSocial") REFERENCES "ObraSocial"("idObraSocial") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
