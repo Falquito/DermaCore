78cb93064cdf11ff9f8de29556280b65f6d75738
LautaAponte <apolauty@gmail.com>
2026-01-19 08:05:33 +0000

    fix: reigstrarconsultas

--- src/components/pacientes/registrarConsultaForm.tsx
+++ src/components/pacientes/registrarConsultaForm.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { CheckedState } from "@/components/ui/checkbox";
+import type { CheckedState } from "@radix-ui/react-checkbox";
 import {
   Select,
   SelectContent,
